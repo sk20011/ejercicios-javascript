799ada9f8bc193e667d9a076bcfc92676bc00f29
Solomon <solomonkasparov2001@gmail.com>
2024-05-16 16:08:27 +0200

    Guardado 16/05/2024

--- Ejercicio-03/ejercicio-03.js
+++ Ejercicio-03/ejercicio-03.js
@@ -1,3 +1,4 @@
+/*Área del triángulo: Encuentra el área de un triángulo con base 6 y altura 4. -> ¿Cómo harías una calculadora de áreas de triángulos? */
 document.addEventListener("DOMContentLoaded", function() {
     let operacionElevada = " Resultado 1 =" + (10**3) + " Resultado 2 =" +(10**-3);
     operacionElevada = "<span style='color:red;'><b>"+operacionElevada+"</b><span/>"
