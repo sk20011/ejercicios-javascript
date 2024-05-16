dc55a6f32502cdffb80d162d6ad40aaeeddcc7e5
Solomon <solomonkasparov2001@gmail.com>
2024-05-16 16:06:05 +0200

    Guarado 16/05/2024

---
+++ Ejercicio-05/Ejercicio-05.js
@@ -0,0 +1,3 @@
+let lado = 2*4;
+let perimetro = lado*2;
+let totalPerimetro = perimetro;
--- Ejercico-01/ejercicio-01.js
+++ Ejercico-01/ejercicio-01.js
@@ -1,3 +1,8 @@
+/*Calcula el resultado de las expresiones:
+Este apartado tiene operaciones numéricas que hay que calcular. Dependiendo de las prioridades o el orden de las operaciones el resultado puede variar.
+
+Operaciones combinadas: 10 + 5 * 2 y (10 + 5) * 2 y 10 + (5 * 2) -> ¿qué observas? */
+
 document.addEventListener('DOMContentLoaded', function() {
     let operacionMatematica = " Resultado = " + (10 + 5 * 2) + " Resultado 2 = " + ((10 + 5) * 2) + " Resultado 3 = " + (10 + (5*3));
     operacionMatematica = "<span style= 'color:red;'><b>"+ operacionMatematica + "</b> </span>"
