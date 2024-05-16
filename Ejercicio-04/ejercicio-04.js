e351587f740b4c43148b0ea57a030277c9bf495a
Solomon <solomonkasparov2001@gmail.com>
2024-05-16 19:46:04 +0200

    Guardado Fecha: 16/05/2024 Hora: 19:46

--- Ejercicio-04/ejercicio-04.js
+++ Ejercicio-04/ejercicio-04.js
@@ -1,10 +1,8 @@
 /*Área del triángulo: Encuentra el área de un triángulo con base 6 y altura 4. -> ¿Cómo harías una calculadora de áreas de triángulos? */
 
-document.addEventListener("DOMContentLoaded", function(){
-    let base = Number.parseInt(prompt(" Introduzca la base del Triángulo: "));
-    let altura = Number.parseInt(prompt(" Introduzca la altura del Triángulo: "));
-    let operacionTriangulo = base * altura / 2;
-    operacionTriangulo = "<span style='color:red;'><b><br>"+operacionTriangulo+"</b></br><style/ span>";
-    document.getElementById("resultado").innerHTML += operacionTriangulo;
+let base = Number.parseInt(prompt(" Introduzca la base del Triángulo: "));
+let altura = Number.parseInt(prompt(" Introduzca la altura del Triángulo: "));
+let operacionTriangulo = base * altura / 2;
+operacionTriangulo = "<span style='color:red;'><b><br>"+operacionTriangulo+"</b></br><style/ span>";
+document.getElementById("resultado").innerHTML += operacionTriangulo;
     
-})
