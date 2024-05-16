b75136dcab1f8774d7a13f1dbf222b9f363088ba
Solomon <solomonkasparov2001@gmail.com>
2024-05-16 19:33:12 +0200

    Guarado 16/05/2024 hora: 19:33

--- Ejercicio-05/Ejercicio-05.js
+++ Ejercicio-05/Ejercicio-05.js
@@ -1,3 +1,9 @@
-let lado = 2*4;
-let perimetro = lado*2;
-let totalPerimetro = perimetro;
+/*Perímetro de un cuadrado: Encuentra el perímetro de un cuadrado con un lado de longitud 8. ¿Cómo harías una calculadora para este dato a partir del lado? ¿Y el área?
+ */
+document.addEventListener("DOMContentLoaded", function(){
+    let lado = 8;
+    let perimetro = lado*4;this.getElementById
+    let totalPerimetro = perimetro;
+    perimetro = "<span style='color:red;'><b><br>"+ perimetro +"</br></b></span>"
+    document.getElementById("resultado").innerHTML +=  perimetro;
+})
--- Ejercico-01/ejercicio-01.js
+++ Ejercico-01/ejercicio-01.js
@@ -2,11 +2,16 @@
 Este apartado tiene operaciones numéricas que hay que calcular. Dependiendo de las prioridades o el orden de las operaciones el resultado puede variar.
 
 Operaciones combinadas: 10 + 5 * 2 y (10 + 5) * 2 y 10 + (5 * 2) -> ¿qué observas? */
-
-document.addEventListener('DOMContentLoaded', function() {
-    let operacionMatematica = " Resultado = " + (10 + 5 * 2) + " Resultado 2 = " + ((10 + 5) * 2) + " Resultado 3 = " + (10 + (5*3));
-    operacionMatematica = "<span style= 'color:red;'><b>"+ operacionMatematica + "</b> </span>"
-    document.getElementById("resultado").innerHTML += operacionMatematica;
+let contadorDeClicks = 0
+document.addEventListener("click", function(){
+    contadorDeClicks++;
+    if(contadorDeClicks <= 1){
+        let operacionMatematica = " Resultado = " + (10 + 5 * 2) + " Resultado 2 = " + ((10 + 5) * 2) + " Resultado 3 = " + (10 + (5*3));
+        operacionMatematica = "<span style= 'color:red;'><b>"+ operacionMatematica + "</b> </span>"
+        document.getElementById("resultado").innerHTML += operacionMatematica;
+    }
 
 })
+
+
 
