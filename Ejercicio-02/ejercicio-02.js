5a8fbd6586df30a603517010025812413ee0b647
Solomon <solomonkasparov2001@gmail.com>
2024-05-16 16:07:20 +0200

    Guardado 16/05/2024

--- Ejercicio-02/ejercicio-02.js
+++ Ejercicio-02/ejercicio-02.js
@@ -1,3 +1,5 @@
+/*Área del círculo: 3.14 * r ** 2, con r igual a 10 -> ¿cómo podemos hacer un programa que reciba el radio r como dato y devuelva el área de la circunferencia? */
+
 document.addEventListener("DOMContentLoaded", function(){
     let pi = 3.14;
     let entradaRadio = Number.parseInt(prompt("Introduzca el valor de la radio"))
