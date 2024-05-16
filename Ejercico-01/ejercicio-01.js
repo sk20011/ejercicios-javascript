9f974d5dbe93dad91168f8080b6552e750ef6c0a
Solomon <solomonkasparov2001@gmail.com>
2024-05-16 19:36:16 +0200

    Guardado hora 19:36 fecha: 16/05/2024

--- Ejercico-01/ejercicio-01.js
+++ Ejercico-01/ejercicio-01.js
@@ -6,7 +6,7 @@ let contadorDeClicks = 0
 document.addEventListener("click", function(){
     contadorDeClicks++;
     if(contadorDeClicks <= 1){
-        let operacionMatematica = " Resultado = " + (10 + 5 * 2) + " Resultado 2 = " + ((10 + 5) * 2) + " Resultado 3 = " + (10 + (5*3));
+        let operacionMatematica = " Resultado 1 = " + (10 + 5 * 2) + " Resultado 2 = " + ((10 + 5) * 2) + " Resultado 3 = " + (10 + (5*3));
         operacionMatematica = "<span style= 'color:red;'><b>"+ operacionMatematica + "</b> </span>"
         document.getElementById("resultado").innerHTML += operacionMatematica;
     }
